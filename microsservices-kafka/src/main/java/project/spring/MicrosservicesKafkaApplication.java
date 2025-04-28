bf20b317864af5f89be2b13e9eaf7dfdf793f436
MiroTragico12 <jorge.santista10@hotmail.com>
2025-04-28 18:56:21 -0300

    inicio da parte de consumer do projeto kafka

--- microsservices-kafka/src/main/java/project/spring/MicrosservicesKafkaApplication.java
+++ microsservices-kafka/src/main/java/project/spring/MicrosservicesKafkaApplication.java
@@ -2,8 +2,10 @@ package project.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class MicrosservicesKafkaApplication {
 
 	public static void main(String[] args) {
--- project-springboot-restfulapi/src/main/java/project/spring/services/kafka/producer/BoletoProducer.java
+++ project-springboot-restfulapi/src/main/java/project/spring/services/kafka/producer/BoletoProducer.java
@@ -1,19 +1,11 @@
 package project.spring.services.kafka.producer;
-
-import java.io.File;
-import java.io.IOException;
-import java.time.ZoneOffset;
-
-import org.apache.avro.Schema;                  
-import org.apache.avro.generic.GenericRecord;                 
-import org.apache.avro.generic.GenericRecordBuilder;         
+         
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import project.spring.avro.Boleto;
-import project.spring.dto.response.BoletoResponse;
 
 @Component
 public class BoletoProducer {
@@ -21,11 +13,18 @@ public class BoletoProducer {
     @Value("${spring.kafka.topico-boleto}")
     private String topico;
 
-    // Use GenericRecord, não GenericRecordBuilder
     @Autowired
     private KafkaTemplate<String, Boleto> kafkaTemplate;
 
     public void enviarMensagem(Boleto boleto) {
-        kafkaTemplate.send(topico, boleto);
+        kafkaTemplate.send(topico, getKey(boleto), boleto);
+    }
+    
+    private String getKey(Boleto boleto) {
+    	if(boleto.getCodigoBarras().toString().substring(0,1).equals("2")){
+    		return "chave1";
+    	}
+    	
+    	return "chave2";
     }
 }
